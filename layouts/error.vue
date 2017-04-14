246856f6e4ab0d1d40a6411fbdf0560dc43d57c4
sfdd <sfdd@sfdddeiMac.local>
2017-04-14 16:34:36 +0800

    update port

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,37 +1,48 @@
 <template>
-  <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
-    </nuxt-link>
+  <section container>
+    <div class="error-page">
+      <!--<img src="~assets/img/logo.png"
+             alt="Nuxt.js Logo"
+             class="logo" />-->
+      <h1 class="error-title">
+            {{ error.statusCode }}
+          </h1>
+      <h2 class="error-message">
+            {{ error.message }}
+          </h2>
+      <nuxt-link class="error-button"
+                 to="/"
+                 v-if="error.statusCode === 404">
+        返回首页
+      </nuxt-link>
+    </div>
+
   </section>
 </template>
 <script>
 export default {
-  props: ['error']
+  props: ['error'],
+  layout: 'empty'
 }
 </script>
 
 <style scoped>
-.title
-{
+.error-page {
+  padding: 100px 250px;
+}
+
+.error-title {
   margin-top: 15px;
   font-size: 5em;
 }
-.info
-{
+
+.error-message {
   font-weight: 300;
   color: #9aabb1;
   margin: 0;
 }
-.button
-{
+
+.error-button {
   margin-top: 50px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,75 @@
 <template>
-  <section container>
+  <div>
+    <!--banner 轮播图-->
+    <section class='banner'>
+      <img class="banner__placeholder"
+           src="~assets/img/index/banner.png"
+           alt="banner-img">
+      <ul class="banner__slide">
+        <li class="banner__slide-item"
+            grid='middle,center,1'>
+          <div col>
+            <h1>2017春季 · 成品整装方案</h1>
+            <h3>走心设计，最美表达，预案式整装方案，设计、硬装、软装、设备一体化 <br />低至 ¥1399/m² 起</h3>
+            <nuxt-link to='/detail'
+                       class='Btn -white'>查看详情</nuxt-link>
+          </div>
+        </li>
+      </ul>
+    </section>
 
-  </section>
+    <!--feature 特性-->
+    <section class="feature">
+      <h1>中孚空间 · 预案式成品整装</h1>
+      <h3>
+                    <p>已为您规划好完整的设计预案，从硬装到软装的造价清单，从制图到全景展示，预案中一应俱全。</p>
+                    <p>帮你把关选材搭配，预算控制，氛围意境。选择我们的当季预案，</p>
+                    <p>来匹配您的房型，设计费仅以2折收取</p>
+                  </h3>
+    </section>
+
+    <section class="video">
+      <div ref='video__wrap'
+           class="video__wrap"
+           :style="videoWrapStyle"
+           container>
+        <video src="//video.baogaoyezhu.com/video/case/406/58c79aac6868d.mp4"
+               class="video__el"
+               controls></video>
+      </div>
+
+    </section>
+
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      videoScale: 0.5
+    }
+  },
+  computed: {
+    videoWrapStyle () {
+      return {
+        transform: `translate3d(0,0,0) scale(${this.videoScale})`
+      }
+    }
+  },
+  watch: {
+    '$store.state.ui.st' (newScrollTop) {
+      const el = this.$refs.video__wrap
+      const { top } = el.getBoundingClientRect()
+      let ratio = top / (window.innerHeight)
+      if (ratio < 0) ratio = 0
+      if (ratio >= 1) ratio = 1
+      let scale = (-4 / 9 * ratio) + 1.05
+      if (scale > 1) scale = 1
+      this.videoScale = scale
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style src='./index.css' scoped></style>
