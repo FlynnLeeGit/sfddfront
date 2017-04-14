d148f052fcd1d1520fad70010661a9028b5094d3
sfdd <sfdd@sfdddeiMac.local>
2017-04-14 13:18:47 +0800

    add depoly script

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,68 @@
 <template>
   <footer class="footer">
     <div container>
-      <ul class="footer__list">
-        <li class="footer__list-title">往期</li>
-        <li>2016冬季</li>
-        <li>2016秋季</li>
-        <li>2016秋季</li>
-        <li>2016秋季</li>
-      </ul>
-      <ul class="footer__list">
-        <li class="footer__list-title">关于</li>
+      <section class="footer__nav">
+        <ul class="footer__list">
+          <li class="footer__list-title">往期</li>
+          <li class='footer__item'>
+            <nuxt-link class='Link' to='/old/1'>2016冬季</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/old/2'>2016秋季</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link' to='/old/2'>2016夏季</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/old/2'>2016春季</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/old/2'>热销小区</nuxt-link>
+          </li>
+        </ul>
+        <ul class="footer__list">
+          <li class="footer__list-title">关于</li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/about/1'>招聘</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/about/2'>商务</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/about/3'>业务</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/about/4'>课题</nuxt-link>
+          </li>
+          <li class='footer__item'>
+            <nuxt-link class='Link'  to='/about/5'>致谢</nuxt-link>
+          </li>
+        </ul>
+        <ul class="footer__list">
+          <li class="footer__list-title">关注</li>
+          <li class='footer__item'>
+            <a class='Link' href='javascript:;'>官方微博</a>
+          </li>
+          <li class='footer__item'>
+            <a class='Link' href='javascript:;'>官方微信</a>
+          </li>
+        </ul>
+        <ul class="footer__list">
+          <li class="footer__list-title">24小时客服热线</li>
+          <li class='footer__phone'>
+            400-080-2121
+          </li>
+        </ul>
+      </section>
 
-      </ul>
+      <section class="footer__copy-right">
+        <span>中孚空间装饰设计 © 2016 上海中孚空间 ／</span>
+        <a class="Link" href='//www.miitbeian.gov.cn' target="_blank">
+          沪ICP备12039245号
+        </a>
+        <span>／</span>
+        <a class="Link" href='/any' target='_blank'>站长统计</a>
+      </section>
     </div>
   </footer>
 </template>
