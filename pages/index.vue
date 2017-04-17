d4af4becb961ebac4ce6b22c29db7bd822708c98
sfdd <sfdd@sfdddeiMac.local>
2017-04-17 16:50:51 +0800

    add 2 sections

--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,14 @@
     </section>
 
     <!--feature 特性-->
-    <section class="feature">
+    <section class="feature"
+             container>
       <h1>中孚空间 · 预案式成品整装</h1>
       <h3>
-                              <p>已为您规划好完整的设计预案，从硬装到软装的造价清单，从制图到全景展示，预案中一应俱全。</p>
-                              <p>帮你把关选材搭配，预算控制，氛围意境。选择我们的当季预案，</p>
-                              <p>来匹配您的房型，设计费仅以2折收取</p>
-                            </h3>
+                        <p>已为您规划好完整的设计预案，从硬装到软装的造价清单，从制图到全景展示，预案中一应俱全。</p>
+                        <p>帮你把关选材搭配，预算控制，氛围意境。选择我们的当季预案，</p>
+                        <p>来匹配您的房型，设计费仅以2折收取</p>
+                      </h3>
     </section>
 
     <section class="video"
@@ -67,9 +68,60 @@
       </div>
     </section>
 
+    <section class="beauty"
+             grid='2,middle'>
+      <img col
+           src="~assets/img/index/beauty.png"
+           alt="beauty-bg.png">
+      <div col
+           class='beauty__desc'>
+        <h1>美感 · 低价 · 高质</h1>
+        <p>每季度，我们的设计董事带领总监级设计师会推出当季的整装预案，
+          <br /> 我们的采购部门会基于预案向更多一线供应商比较以确保低价
+        </p>
+      </div>
+    </section>
+
+    <section class="standard -design"
+             container>
+      <img class='standard__img'
+           src="~assets/img/index/house.png"
+           alt="design standard">
+      <img class='standard__number'
+           src="~assets/img/index/01.png"
+           alt="01">
+      <div class="standard__mask"></div>
+      <div class="standard__content">
+        <h2>整装设计标准</h2>
+        <p>每套整装方案中，我们充分考虑了十二类家具收纳、十条家居动线、六类色彩搭配、核心风格与主打元素。我们基于每个风格，我们会主导三个近似的搭配，并提供软装包（可拆项选择）。帮助您第一时间明确造价、材料、施工内容、设备与软装。</p>
+        <nuxt-link class="Btn -black"
+                   to='/1'>深入了解</nuxt-link>
+      </div>
+    </section>
+
+    <div class="idx-line -left -first">01</div>
+
+    <section class="standard -quality"
+             container>
+      <img class="standard__img"
+           src="~assets/img/index/mac.png"
+           alt="mac">
+      <img class="standard__number"
+           src="~assets/img/index/02.png"
+           alt="02">
+      <div class="standard__mask"></div>
+      <div class="standard__content">
+        <h2>质量控制标准</h2>
+        <p>我们作为专业的项目总承包商，深谙质量责任之重大，在以下六件事上不敢有半点马虎。自有施工班组成熟稳定、严审制图（深化）精度、先放线后施工、行业领先的工艺要求、稳定成熟的建材配套与尽心尽责的质检管理；并通过我们的协同管理系统，各司其职，统一汇报，为用户提供远超国标的质量标准。</p>
+        <nuxt-link class="Btn -black"
+                   to='/2'>深入了解</nuxt-link>
+      </div>
+    </section>
+
+    <div class="idx-line -right -second">02</div>
+    <section></section>
   </div>
 </template>
 
 <script src='./index.js'></script>
-
 <style src='./index.css' scoped></style>
