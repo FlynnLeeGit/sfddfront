7b3065bb1b93c14caa6ffeb463331c3fd68432e2
sfdd <sfdd@sfdddeiMac.local>
2017-04-18 10:34:18 +0800

    modify viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,14 +9,14 @@ module.exports = {
   dev: isDev,
   cache: !isDev,
   head: {
-    title: 'starter',
+    title: 'SFDD',
     meta: [
       {
         charset: 'utf-8'
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        content: 'width=1280'
       },
       {
         hid: 'description',
