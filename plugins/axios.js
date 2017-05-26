2d3be9e13e01f584cf52862cc468886a05bef6f0
sfdd <sfdd@sfdddeiMac.local>
2017-05-26 15:06:34 +0800

    update axios url node

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,15 +3,6 @@ import configApi from './config-api'
 
 // const isProd = process.env.NODE_ENV === 'production'
 
-const opts = {}
-
-// 服务器端请求使用axios时加入请求域名
-if (process.SERVER_BUILD) {
-  opts.baseURL = process.env.REQ_HOST
-}
-
-const axiosInstance = axios.create(opts)
-
 export default {
   get (url, options) {
     const key = JSON.stringify({ ...options, url })
@@ -19,7 +10,11 @@ export default {
       console.log(`使用api缓存${url},${JSON.stringify(options)}`) // eslint-disable-line
       return Promise.resolve(configApi.cached.get(key))
     }
-    return axiosInstance.get(url, options).then(res => {
+    // 服务器端请求使用axios时加入请求域名
+    if (process.SERVER_BUILD) {
+      url = process.env.REQ_HOST + url
+    }
+    return axios.get(url, options).then(res => {
       if (configApi.cached) configApi.cached.set(key, res)
       return res
     })
